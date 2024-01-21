9e85233ebe40b8ff644bc2b6ff4fbe7bd620f631
Harshit Gupta <hersheyg0105@gmail.com>
2024-01-21 10:10:41 -0600

    Nav Bar is displayed

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,33 @@ import Nav from "./components/Nav";
 
 const App = () => (
   <main className="relative text-black">
-    <Nav/>
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero/>
+      <Hero />
     </section>
     <section className="padding">
       <PopularProducts />
     </section>
     <section className="padding">
-      <SuperQuality/>
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      <Services/>
+      <Services />
     </section>
     <section className="padding">
-      <SpecialOffer/>
+      <SpecialOffer />
     </section>
     <section className="bg-blue-50 padding">
-      <CustomerReviews/>
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe/>
+      <Subscribe />
     </section>
     <section className="padding bg-black padding-x padding-t pb-8">
-      <Footer/>
+      <Footer />
     </section>
   </main>
-)
+);
 
 export default App;
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} />
+        </a>
+        <ul className="flex-1 flex justify-center gap-16">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-sans leading-normal text-lg text-slate-700"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
