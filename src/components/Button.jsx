295603114dd70ae0514d2696eeed322ffddb3dff
Harshit Gupta <hersheyg0105@gmail.com>
2024-01-22 23:29:10 -0600

    quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,15 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className=" flex justify-center items-center gap-1 px-7 py-4 m-4 font-montserrat text-lg leading-none border rounded-full text-white bg-coral-red border-coral-red">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className=" ml-3 rounded-full"
-        w-5
-        h-5
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className=" ml-3 rounded-full"
+          w-5
+          h-5
+        />
+      )}
     </button>
   );
 };
