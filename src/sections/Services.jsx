948ccc3e20178f53e81ba900a55dcd8125e80188
Harshit Gupta <hersheyg0105@gmail.com>
2024-01-24 23:23:36 -0600

    services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section className=" max-container flex justify-center align-middle flex-wrap gap-6">
+      {services.map((service, index) => (
+        <ServiceCard key={service.label} {...service}></ServiceCard>
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
